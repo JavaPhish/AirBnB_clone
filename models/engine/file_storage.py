038c99639e34ade68674274c0d20d256ebb39475
Carter C <cclements3150@gmail.com>
2020-06-30 23:14:19 -0400

    [CREATE] Json handling functions

---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" JSON storage handler """
+
+
+import os.path
+import json
+import copy
+
+
+class FileStorage:
+    """ Filestorage class """
+
+    """ Path the the storage file """
+    __file_path = 'file.json'
+
+    """ This will store objects be <class name>.id """
+    __objects = {}
+
+    def all(self):
+        """ Returns all objects by id (__objects) """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """ adds a new id into __objects """
+        k_name = "{}.{}".format(str(obj.__class__.__name__), obj.id)
+
+        FileStorage.__objects[k_name] = obj.to_dict()
+
+    def save(self):
+        """ Save all current data to a json file """
+        with open(FileStorage.__file_path, 'w') as j_file:
+            json.dump(FileStorage.__objects, j_file)
+
+    def reload(self):
+        """ Loads all data from the json file """
+        if (os.path.isfile(self.__file_path) is True):
+            with open(self.__file_path, 'r') as j_data:
+                FileStorage.__objects = json.load(j_data)
