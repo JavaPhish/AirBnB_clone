a82f8da75f7c85a0fd49f1e33c7ca8a25b535688
Carter C <cclements3150@gmail.com>
2020-06-30 23:15:07 -0400

    Updated to work with file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,32 @@
 
 import uuid
 from datetime import datetime
+from models import storage
+import copy
 
 
 class BaseModel:
     """ Base model class """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initial stuff """
-        self.id = str(uuid.uuid4())
+
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+
+        if (kwargs is not None and len(kwargs) is not 0):
+            self.id = kwargs['id']
+
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    exec("self.{} = value".format(key))
+
+            form = '%Y-%m-%dT%H:%M:%S.%f'
+            self.created_at = datetime.strptime(kwargs['created_at'], form)
+            self.updated_at = datetime.now()
+        else:
+            storage.new(self)
 
     def __str__(self):
         """ Prints formatted string about current class """
@@ -23,10 +39,11 @@ class BaseModel:
 
     def to_dict(self):
         """ Prep for JSON serial and de-serialization """
-        n_dict = self.__dict__
-        cls_name = self.__class__.__name__
 
-        n_dict['__class__'] = cls_name
+        s_copy = copy.deepcopy(self)
+        n_dict = s_copy.__dict__
+
+        n_dict['__class__'] = self.__class__.__name__
         n_dict['created_at'] = self.created_at.isoformat()
         n_dict['updated_at'] = self.updated_at.isoformat()
 
@@ -34,4 +51,6 @@ class BaseModel:
 
     def save(self):
         """ Currently just updates the updated at time """
+
         self.updated_at = datetime.now()
+        storage.save()
